a1d89a890b7523d272da8b27bbe6fed4476be144
Rukhmani <rnar@uni-bonn.de>
2024-02-20 16:18:34 +0100

    needs to becompleted

--- Day2/Session2/src/main.cpp
+++ Day2/Session2/src/main.cpp
@@ -1,11 +1,23 @@
-// Development Plan: Commit the code each time a step is completed.
-// [ ] Write and build the boilerplate code for Arduino (#include<Arduino.h>, setup(), loop())
-// [ ] Open the simulator
-// [ ] Make the red LED flash on and off once every 50 milliseconds.
-// [ ] Make the red LED flash on and off only if one of the buttons is being pressed.
-// [ ] Turn on an LED when the button of a corresponding color is held down.
-// [ ] The blue LED should be different; it should turn off when its button is pressed, not on.
-// [ ] The green LED should be a switch; when its button is pressed, it should change from staying on to staying off, then go back the next time it's pressed.
-// [ ] When the red button is pressed, it should turn on for a couple of seconds, then automatically turn back off.
-// [ ] All buttons' LEDs should respond as soon as they are pressed.
+#include<Arduino.h>
+const int redLED =3;
+bool isOn = true;
+int redbuttonstate = digitalRead(9);
+int REDBUTTON = 11;
 
+void setup(){
+    pinMode(redLED , OUTPUT);
+    pinMode(REDBUTTON , INPUT_PULLUP);
+}
+void loop(){
+    digitalWrite(redLED, LOW);
+    delay(50);
+    digitalWrite(redLED, HIGH);
+    delay(50);
+}
+
+void loop2(){
+    if (redbuttonstate==isOn){
+    pinMode(5, INPUT_PULLUP);
+    digitalWrite(5, HIGH);
+    }
+}
